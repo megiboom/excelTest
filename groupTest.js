3ddf5c31be931619b0129278fee37d70b75376e0
unknown <megiboom@naver.com>
2020-11-12 16:16:21 +0900

    그룹화 완료

--- groupTest.js
+++ groupTest.js
@@ -1,37 +1,26 @@
-var groupBy = require('json-groupby')
 var json = require('./test.json')
 
-var groupJ = groupBy(json.result,['groupId'],['deviceId','channelNo'])
-//console.log(groupJ)
-
-var obj = {
-    "deviceId": 117,
-    "groupId": 1,
-    "channelNo": 3,
-    "current": 100,
-    "activePowerQty": 200,
-    "regDate": "2020-11-12 10:00:00"
-}
-//json.result.push(obj)
-var result2 = json.result.reduce((r,{groupId: name, ...object})=>{
-    //console.log(groupId)
-    var temp = r.find(o => {
-        o.name === name
-    });
-    if(!temp) r.push(temp = {name, children: []});
-    
-    temp.children.push(object);
-    return r;
-},[])
-console.log(result2)
-
-var array = [{ name: "cat", value: 17, group: "animal" }, { name: "dog", value: 6, group: "animal" }, { name: "snak", value: 2, group: "animal" }, { name: "tesla", value: 11, group: "car" }, { name: "bmw", value: 23, group: "car" }],
-    result = array.reduce((r, { group: name, ...object }) => {
-        var temp = r.find(o => o.name === name);
-        if (!temp) r.push(temp = { name, children: [] });
-        temp.children.push(object);
-        //console.log(r)
+function groupBy(arr){
+    const result = arr.result.reduce((r, { moduleId: moduleId,groupId:groupId,groupName:groupName, ...object }) => {
+        var temp = r.find(o => o.moduleId === moduleId);
+        if (!temp) r.push(temp = { moduleId,groupId,groupName, data: [] });
+        temp.data.push(object);
         return r;
     }, []);
-    
-//console.log(result)
+        
+    const result1 = result.reduce((r,{groupId:groupId,groupName:groupName, ...object})=>{
+        var temp = r.find(o => o.groupId === groupId);
+        if (!temp) r.push(temp = { groupId,groupName, moduleList: [] });
+        temp.moduleList.push(object);
+        return r;
+    },[])
+
+    return result1;
+}
+
+const gArr = groupBy(json);
+//console.log(gArr.length);
+console.log(gArr[0].moduleList.length);
+//console.log(JSON.stringify(gArr));
+
+module.exports.groupBy=groupBy;
--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 var Excel = require('exceljs');
-var workbook = new Excel.Workbook();
-var testData = require('./test.json')
+var groupJson = require('./groupTest');
+var moduleData = require('./test.json')
 
-workbook.xlsx.readFile('template.xlsx')
+var workbook = new Excel.Workbook();
+
+var color=['FFC6E0B4','FFFFFF99','FFFFE699','FFC6E0B4','FFCCFFCC','FFBDD7EE']
+
+function numToSSColumn(num){
+  var s = '', t;
+  var n = num;
+  while (num > 0) {
+    t = (num - 1) % 26;
+    s = String.fromCharCode(65 + t) + s;
+    num = (num - t)/26 | 0;
+  }
+  return s || undefined;
+}
+
+function makeExcel(data){
+    workbook.xlsx.readFile('template.xlsx')
     .then(function() {
         var worksheet = workbook.getWorksheet(1);
 
-        var rowNum = 2;
-        var cellNum = 3;
-        var moduleLength = 4;
-        var groupLength = 2;
+        var rowNum = 3;
+        var cellNum = 2;
+        var groupLength = data.length;
 
         for(var j =0; j<groupLength; j++){
             
-            cellNum=cellNum+(3*moduleLength)*j;
+            var moduleLength = 10;
+            var moduleLength = data[j].moduleList.length;
+
             /* 대분류 시작 */
             //시작행, 시작열, 끝행, 끝열
             worksheet.mergeCells(rowNum, cellNum, rowNum, cellNum+3*moduleLength-1);
@@ -21,20 +38,24 @@ workbook.xlsx.readFile('template.xlsx')
                 vertical: 'middle', 
                 horizontal: 'center'
             }
+
+            const randomColor=color[j%color.length];
+            
             worksheet.getCell(rowNum,cellNum).fill = {
                 type:'pattern',
                 pattern:'solid',
-                fgColor:{argb:'CCFFEE'}
+                fgColor:{argb:randomColor}
+                //fgColor:{argb:'CCFFEE'}
             }
             worksheet.getCell(rowNum,cellNum).border = {top: {style:'medium'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
 
             var row = worksheet.getRow(rowNum);        
-            row.getCell(cellNum).value='154kV S/S (154kV)'  //대분류 명명
+            row.getCell(cellNum).value='154kV S/S (154kV)'+j  //대분류 명명
             row.getCell(cellNum).font={bold:true}
             /* 대분류 끝 */
 
             /* 중분류 시작 */
-            for(var i=0; i<moduleLength;i++){//0 (3,3,2,5) 1 (3,6,2,8) 2 (3,9,2,11) 3>5>8>11
+            for(var i=0; i<moduleLength;i++){
 
                 worksheet.mergeCells(rowNum+1, cellNum+3*i, rowNum+1, (cellNum+3*i)+2);
                 worksheet.getCell(rowNum+1,cellNum+3*i).alignment = {
@@ -44,7 +65,7 @@ workbook.xlsx.readFile('template.xlsx')
                 worksheet.getCell(rowNum+1,cellNum+3*i).fill = {
                     type:'pattern',
                     pattern:'solid',
-                    fgColor:{argb:'CCFFEE'}
+                    fgColor:{argb:randomColor}
                 }
                 worksheet.getCell(rowNum+1,cellNum+3*i).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
             
@@ -55,83 +76,88 @@ workbook.xlsx.readFile('template.xlsx')
                 /* 소분류 시작 */
                 row = worksheet.getRow(rowNum+2);
                 row.getCell(cellNum+3*i).value='전류[A]'
-                worksheet.getCell(rowNum+2,cellNum+3*i).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
+                worksheet.getCell(rowNum+2,cellNum+3*i).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'hair'}};
                 worksheet.getCell(rowNum+2,cellNum+3*i).fill = {
                     type:'pattern',
                     pattern:'solid',
-                    fgColor:{argb:'CCFFEE'}
+                    fgColor:{argb:randomColor}
                 }
                 row.getCell(cellNum+3*i+1).value='지침'
-                worksheet.getCell(rowNum+2,cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
+                worksheet.getCell(rowNum+2,cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'thin'},right: {style:'hair'}};
                 worksheet.getCell(rowNum+2,cellNum+3*i+1).fill = {
                     type:'pattern',
                     pattern:'solid',
-                    fgColor:{argb:'CCFFEE'}
+                    fgColor:{argb:randomColor}
                 }
                 row.getCell(cellNum+3*i+2).value='전력량'
-                worksheet.getCell(rowNum+2,cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
+                worksheet.getCell(rowNum+2,cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'thin'},right: {style:'thin'}};
                 worksheet.getCell(rowNum+2,cellNum+3*i+2).fill = {
                     type:'pattern',
                     pattern:'solid',
-                    fgColor:{argb:'CCFFEE'}
+                    fgColor:{argb:randomColor}
                 }
 
                 //합계
                 row = worksheet.getRow(rowNum+28);
-                var colNum = (cellNum+3*i+2 + 9).toString(36).toUpperCase();
-                row.getCell(cellNum+3*i+2).value={ formula: "SUM("+colNum+5+","+colNum+29+")"}
-                row.getCell(cellNum+3*i+2).numFmt = '#,##';
+                var colNum = numToSSColumn(cellNum+3*i+2)
+                row.getCell(cellNum+3*i+2).value={ formula: "SUM("+colNum+(rowNum+3)+","+colNum+(rowNum+27)+")"}
+                row.getCell(cellNum+3*i+2).numFmt = '#,##.0';
                 row.getCell(cellNum+3*i+2).font={bold:true,color: { argb: 'FFFF0000' }}
-                row.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
-                row.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
-                row.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
+                row.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'hair'}};
+                row.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'hair'}};
+                row.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'thin'}};
                 //평균
                 row = worksheet.getRow(rowNum+29);
-                var colNum = (cellNum+3*i+2 + 9).toString(36).toUpperCase();
-                row.getCell(cellNum+3*i+2).value={ formula: "Average("+colNum+5+","+colNum+29+")"}
-                row.getCell(cellNum+3*i+2).numFmt = '#,##';
+                var colNum = numToSSColumn(cellNum+3*i+2)
+                row.getCell(cellNum+3*i+2).value={ formula: "Average("+colNum+(rowNum+3)+","+colNum+(rowNum+27)+")"}
+                row.getCell(cellNum+3*i+2).numFmt = '#,##.0';
                 row.getCell(cellNum+3*i+2).font={bold:true,color: { argb: 'FF800080' }}
-                row.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
-                row.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
-                row.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
+                row.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'hair'}};
+                row.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'hair'}};
+                row.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'thin'}};
                 //최대전력
                 row = worksheet.getRow(rowNum+30);
-                var colNum = (cellNum+3*i+2 + 9).toString(36).toUpperCase();
-                row.getCell(cellNum+3*i+2).value={ formula: "MAX("+colNum+5+","+colNum+29+")"}
-                row.getCell(cellNum+3*i+2).numFmt = '#,##';
+                var colNum = numToSSColumn(cellNum+3*i+2)
+                row.getCell(cellNum+3*i+2).value={ formula: "MAX("+colNum+(rowNum+3)+","+colNum+(rowNum+27)+")"}
+                row.getCell(cellNum+3*i+2).numFmt = '#,##.0';
                 row.getCell(cellNum+3*i+2).font={bold:true,color: { argb: 'FF0000FF' }}
-                row.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
-                row.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
-                row.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
+                row.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'hair'}};
+                row.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'hair'}};
+                row.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'thin'}};
                 // 부하량
                 row = worksheet.getRow(rowNum+31);
                 row.getCell(cellNum+3*i+2).font={bold:true,color: { argb: 'FFFF00FF' }}
-                row.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'medium'},right: {style:'thin'}};
-                row.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'medium'},right: {style:'thin'}};
-                row.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'medium'},right: {style:'thin'}};
+                row.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'medium'},right: {style:'hair'}};
+                row.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'medium'},right: {style:'hair'}};
+                row.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'hair'},bottom: {style:'medium'},right: {style:'thin'}};
                 /* 소분류 끝 */
 
 
-                var rows = worksheet.getRows(5,25);
+                var rows = worksheet.getRows(rowNum+3,rowNum+22);
             
                 rows.forEach((value) => {
-                    value.getCell(cellNum+3*i).value = 100000; //전류
-                    value.getCell(cellNum+3*i).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
-                
+                    value.getCell(cellNum+3*i).value = 100000+j; //전류
+                    value.getCell(cellNum+3*i).border = {top: {style:'hair'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'hair'}};
+                    value.getCell(cellNum+3*i).numFmt='#,##'         
                     //지침
-                    value.getCell(cellNum+3*i+1).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
+                    value.getCell(cellNum+3*i+1).border = {top: {style:'hair'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'hair'}};
 
-                    value.getCell(cellNum+3*i+2).value = 100000.3; //전력량
-                    value.getCell(cellNum+3*i+2).border = {top: {style:'thin'},left: {style:'thin'},bottom: {style:'thin'},right: {style:'thin'}};
-                
+                    value.getCell(cellNum+3*i+2).value = 100000+i; //전력량
+                    value.getCell(cellNum+3*i+2).border = {top: {style:'hair'},left: {style:'hair'},bottom: {style:'hair'},right: {style:'thin'}};
+                    value.getCell(cellNum+3*i+2).numFmt='#,##.0'
                     value.commit();
                 })
             }
             row.commit();
             /* 중분류 끝 */
-            
+            cellNum=cellNum+(3*moduleLength);
         }
 
-        
-        return workbook.xlsx.writeFile('new.xlsx');
+        return "complete";
+        //return workbook.xlsx.writeFile('new.xlsx');
+    }).then(()=>{
+        workbook.xlsx.writeFile('new.xlsx');
     })
+}
+
+makeExcel(groupJson.groupBy(moduleData));
